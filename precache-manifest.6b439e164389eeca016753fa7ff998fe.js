9f86995fac00b232dc91131886a5b2ed048b5b19
LoginmyWorld <98610167+Nagesh1001@users.noreply.github.com>
2022-05-28 12:28:26 +0530

    Updates

---
+++ precache-manifest.6b439e164389eeca016753fa7ff998fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62ee8593c535870e5daf",
+    "url": "/oslash-assignment-movie-ticket-booking/static/css/main.a8372a53.chunk.css"
+  },
+  {
+    "revision": "62ee8593c535870e5daf",
+    "url": "/oslash-assignment-movie-ticket-booking/static/js/main.62ee8593.chunk.js"
+  },
+  {
+    "revision": "ad80ed27c5e0e0ba8dfc",
+    "url": "/oslash-assignment-movie-ticket-booking/static/js/1.ad80ed27.chunk.js"
+  },
+  {
+    "revision": "d26f08012e4e32f25290",
+    "url": "/oslash-assignment-movie-ticket-booking/static/js/2.d26f0801.chunk.js"
+  },
+  {
+    "revision": "358be9af2e59089b8cb7",
+    "url": "/oslash-assignment-movie-ticket-booking/static/js/runtime~main.358be9af.js"
+  },
+  {
+    "revision": "d6c6208c0672fc8b6a725862be600627",
+    "url": "/oslash-assignment-movie-ticket-booking/index.html"
+  }
+];
